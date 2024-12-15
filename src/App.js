3c43379bb7cd2a1d9c883b7aa337b30391c2e90d
kimbi619 <kimbidarl@gmail.com>
2024-12-15 14:34:22 -0500

    no

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AppWrapper from './app/AppWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <AppWrapper />
+    </div>
+  );
+}
+
+export default App;
